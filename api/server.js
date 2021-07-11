f6671f93d216503cb7fb62c7bafe169c47482678
Tristan Miller <tristanmiller89@gmail.com>
2021-07-10 20:43:02 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
+const express = require('express');
+const server = express();
+
+server.use(express.json());
 // require your posts router and connect it here
+const postsRouter = require('./posts/posts-router');
+
+server.use('/api/posts', postsRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+  <h2>Unit 4 Module 1 Assignment 2 API</h2>
+  <p>Welcome to the server</p>
+  `);
+});
+
+module.exports = server;
